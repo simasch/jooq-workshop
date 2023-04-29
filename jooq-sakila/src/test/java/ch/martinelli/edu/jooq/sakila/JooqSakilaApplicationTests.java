c12916c8384095c2e7ae32025ca7d6a6291b0dad
Simon Martinelli <simon@72.services>
2023-04-29 09:08:51 +0200

    initial commit

---
+++ jooq-sakila/src/test/java/ch/martinelli/edu/jooq/sakila/JooqSakilaApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.martinelli.edu.jooq.sakila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqSakilaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
