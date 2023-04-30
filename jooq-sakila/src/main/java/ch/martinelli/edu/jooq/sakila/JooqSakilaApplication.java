8f26bbf75fc3f11205645be0a6b9f5670775737d
Simon Martinelli <simon@72.services>
2023-04-30 09:51:32 +0200

    Initial commit

---
+++ jooq-sakila/src/main/java/ch/martinelli/edu/jooq/sakila/JooqSakilaApplication.java
@@ -0,0 +1,13 @@
+package ch.martinelli.edu.jooq.sakila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqSakilaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooqSakilaApplication.class, args);
+    }
+
+}
