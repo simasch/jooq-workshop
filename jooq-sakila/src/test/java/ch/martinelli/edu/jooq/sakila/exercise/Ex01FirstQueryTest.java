a21e443f4f90c7cf037cd7778eaa9b3115db1031
Simon Martinelli <simon@72.services>
2023-05-08 16:35:24 +0200

    Added select from dual

--- jooq-sakila/src/test/java/ch/martinelli/edu/jooq/sakila/exercise/Ex01FirstQueryTest.java
+++ jooq-sakila/src/test/java/ch/martinelli/edu/jooq/sakila/exercise/Ex01FirstQueryTest.java
@@ -1,6 +1,7 @@
 package ch.martinelli.edu.jooq.sakila.exercise;
 
 import ch.martinelli.edu.jooq.sakila.JooqTestcontainersTest;
+import org.jooq.impl.DSL;
 import org.junit.jupiter.api.Test;
 
 public class Ex01FirstQueryTest extends JooqTestcontainersTest {
@@ -8,5 +9,9 @@ public class Ex01FirstQueryTest extends JooqTestcontainersTest {
     @Test
     void query() {
         title("A simple query");
+
+        var result = dsl.selectFrom(DSL.dual()).fetch();
+
+        println(result);
     }
 }
