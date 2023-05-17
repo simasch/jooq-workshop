5abb84fa9606c746f0478a08954ff70409155dde
Simon Martinelli <simon@72.services>
2023-05-17 16:33:31 +0200

    Ex06

--- jooq-sakila/src/test/java/ch/martinelli/edu/jooq/sakila/exercise/Ex07LockingAndTrxTest.java
+++ jooq-sakila/src/test/java/ch/martinelli/edu/jooq/sakila/exercise/Ex07LockingAndTrxTest.java
@@ -1,7 +1,24 @@
 package ch.martinelli.edu.jooq.sakila.exercise;
 
 import ch.martinelli.edu.jooq.sakila.JooqTestcontainersTest;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static ch.martinelli.edu.jooq.sakila.db.tables.Rental.RENTAL;
 
 public class Ex07LockingAndTrxTest extends JooqTestcontainersTest {
-    
+
+    @Test
+    void optimistic_locking() {
+        dsl
+                .selectFrom(RENTAL)
+                .where(RENTAL.RENTAL_ID.eq(854L))
+                .fetchOptional()
+                .ifPresent(rental -> {
+                    rental.setRentalDate(LocalDateTime.now());
+                    rental.store();
+                });
+    }
+
 }
