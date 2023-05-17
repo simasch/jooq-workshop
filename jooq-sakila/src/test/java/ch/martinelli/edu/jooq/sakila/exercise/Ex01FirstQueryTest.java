21f104b596fdc5c98462abc0e9b55df4662c8a70
Simon Martinelli <simon@72.services>
2023-05-17 11:16:08 +0200

    Ex01

--- jooq-sakila/src/test/java/ch/martinelli/edu/jooq/sakila/exercise/Ex01FirstQueryTest.java
+++ jooq-sakila/src/test/java/ch/martinelli/edu/jooq/sakila/exercise/Ex01FirstQueryTest.java
@@ -1,8 +1,21 @@
 package ch.martinelli.edu.jooq.sakila.exercise;
 
 import ch.martinelli.edu.jooq.sakila.JooqTestcontainersTest;
+import ch.martinelli.edu.jooq.sakila.db.tables.Actor;
+import ch.martinelli.edu.jooq.sakila.db.tables.Category;
+import ch.martinelli.edu.jooq.sakila.db.tables.Film;
+import ch.martinelli.edu.jooq.sakila.db.tables.FilmCategory;
 import org.jooq.impl.DSL;
 import org.junit.jupiter.api.Test;
+
+import static ch.martinelli.edu.jooq.sakila.db.Tables.FILM_ACTOR;
+import static ch.martinelli.edu.jooq.sakila.db.tables.Actor.ACTOR;
+import static ch.martinelli.edu.jooq.sakila.db.tables.Category.CATEGORY;
+import static ch.martinelli.edu.jooq.sakila.db.tables.Film.FILM;
+import static ch.martinelli.edu.jooq.sakila.db.tables.FilmCategory.FILM_CATEGORY;
+import static ch.martinelli.edu.jooq.sakila.db.tables.Language.LANGUAGE;
+import static org.jooq.impl.DSL.count;
+import static org.jooq.impl.DSL.select;
 
 public class Ex01FirstQueryTest extends JooqTestcontainersTest {
 
@@ -11,6 +24,62 @@ public class Ex01FirstQueryTest extends JooqTestcontainersTest {
         title("A simple query");
 
         var result = dsl.selectFrom(DSL.dual()).fetch();
+
+        println(result);
+    }
+
+    @Test
+    void all_actors() {
+        var result = dsl.selectFrom(ACTOR).fetch();
+
+        println(result);
+    }
+
+    @Test
+    void all_films() {
+        var result = dsl
+                .select(FILM.TITLE, LANGUAGE.NAME)
+                .from(FILM)
+                .join(LANGUAGE).on(LANGUAGE.LANGUAGE_ID.eq(FILM.LANGUAGE_ID))
+                .fetch();
+
+        println(result);
+    }
+
+    @Test
+    void all_films_implicit() {
+        var result = dsl
+                .select(FILM.TITLE, FILM.filmLanguageIdFkey().NAME)
+                .from(FILM)
+                .fetch();
+
+        println(result);
+    }
+
+    @Test
+    void all_actors_with_number_of_films() {
+        var result = dsl
+                .select(FILM_ACTOR.actor().FIRST_NAME,
+                        FILM_ACTOR.actor().LAST_NAME,
+                        count(FILM_ACTOR.FILM_ID))
+                .from(FILM_ACTOR)
+                .groupBy(FILM_ACTOR.actor().FIRST_NAME, FILM_ACTOR.actor().LAST_NAME)
+                .fetch();
+
+        println(result);
+    }
+
+    @Test
+    void multiset() {
+        var result = dsl
+                .select(CATEGORY.NAME,
+                        DSL.multiset(
+                                select(FILM.TITLE)
+                                        .from(FILM)
+                                        .join(FILM_CATEGORY).on(FILM_CATEGORY.FILM_ID.eq(FILM.FILM_ID)
+                                        )))
+                .from(CATEGORY)
+                .fetch();
 
         println(result);
     }
