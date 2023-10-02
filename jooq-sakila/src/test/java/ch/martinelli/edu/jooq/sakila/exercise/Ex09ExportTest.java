a95cc51036f855c33ad2755a8080997b34e65f95
Simon Martinelli <simon@72.services>
2023-10-02 14:47:03 +0200

    Added Ex09ExportTest

---
+++ jooq-sakila/src/test/java/ch/martinelli/edu/jooq/sakila/exercise/Ex09ExportTest.java
@@ -0,0 +1,16 @@
+package ch.martinelli.edu.jooq.sakila.exercise;
+
+import ch.martinelli.edu.jooq.sakila.JooqTestcontainersTest;
+import org.junit.jupiter.api.Test;
+
+import static ch.martinelli.edu.jooq.sakila.db.tables.Category.CATEGORY;
+
+public class Ex09ExportTest extends JooqTestcontainersTest {
+
+    @Test
+    void export_to_csv() {
+        var csv = dsl.selectFrom(CATEGORY).fetch().formatCSV();
+
+        println(csv);
+    }
+}
